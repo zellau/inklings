34866d6b1bcabb94bdaf726318718c315ca2b662
Zella <zella.urquhart@gmail.com>
2024-02-10 20:01:18 -0800

    Make header using swifter

--- Sources/main.swift
+++ Sources/main.swift
@@ -4,14 +4,21 @@
 import Swifter
 let server = HttpServer()
 server["/octopus.png"] = shareFile("./octopus.png")
+server["/inklings.css"] = shareFile("./inklings.css")
 server["/"] = shareFile("./index.html")
-// server["/my_html"] = scopes { 
-//   html {
-//     body {
-//       h1 { inner = "hello" }
-//     }
-//   }
-// }
+server["/a"] = scopes { 
+    html {
+      header {
+        link {
+          rel = "stylesheet"
+          href = "inklings.css"
+        }
+      }
+      body {
+        makeHeader();
+      }
+    }
+  }
 
 try server.start(8081)
 print("Server has started ( port = \(try server.port()) ). Try to connect now...")
@@ -21,3 +28,51 @@ print("Server has started ( port = \(try server.port()) ). Try to connect now...
 //bleh
 import Dispatch
 DispatchSemaphore(value: 0).wait()
+
+func makeHeader() -> () {
+  return div {
+    h1 {
+      img {
+        src = "octopus.png"
+        width = "100"
+        height = "100"
+      }
+      span {
+        inner = "Inklings"
+      }
+    }
+    nav {
+      a {
+        href="/"
+        inner="Home"
+      }
+      span { inner = "|" }
+      a {
+        href="/bookshelf"
+        inner="Boookshelf"
+      }
+      span { inner = "|" }
+      a {
+        href="/notebook"
+        inner="Notebook"
+      }
+      span { inner = "|" }
+      a {
+        href="/account"
+        inner="Account"
+      }
+      a {
+        href="/search"
+        classs = "right-align"
+        inner="Search"
+      }
+    }
+  }
+}
+
+// func makeHeader(request: HTTPRequest) -> HTTPResponse {
+//     return h1 {
+//         img { src = "octopus.png"}
+//     }
+//   //return "<h1> <img src='octopus.png' width='100' height='100' alt='🐙' /> Inklings</h1>";
+// }
