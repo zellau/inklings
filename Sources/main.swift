f5ef55fdcc69993c4fb9e6c6d73ba3b4a1f98813
Zella <zella.urquhart@gmail.com>
2024-02-17 13:21:09 -0800

    Add notebooks page

--- Sources/main.swift
+++ Sources/main.swift
@@ -7,35 +7,31 @@ import SQLite
 let server = HttpServer()
 server["/octopus.png"] = shareFile("./octopus.png")
 server["/inklings.css"] = shareFile("./inklings.css")
-server["/"] = shareFile("./index.html")
-server["/a"] = scopes { 
+server["/a"] = shareFile("./index.html")
+server["/"] = scopes { 
     html {
       header {
         addStylesheet();
       }
       body {
         makeHeader();
-         do {
-            let pages = Table("pages");
-            let title = Expression<String>("title")
-            let body = Expression<String>("body")
-
-            let db = try Connection("inklings.sqlite3");
-
-            for page in try db.prepare(pages) {
-                h2 {
-                    inner = page[title]
-                }
-                p {
-                    inner = page[body]
-                }
-            }
-         } catch {
-            //log this probably
-         }
+        showPage(1);
       }
     }
   }
+
+  server["/notebooks"] = scopes { 
+    html {
+      header {
+        addStylesheet();
+      }
+      body {
+        makeHeader();
+        showNotebooks(forUser: 1);
+      }
+    }
+  }
+
 
 try server.start(8081)
 print("Server has started ( port = \(try server.port()) ). Try to connect now...")
@@ -77,8 +73,8 @@ func makeHeader() -> () {
       }
       span { inner = "|" }
       a {
-        href="/notebook"
-        inner="Notebook"
+        href="/notebooks"
+        inner="Notebooks"
       }
       span { inner = "|" }
       a {
@@ -94,9 +90,61 @@ func makeHeader() -> () {
   }
 }
 
-// func makeHeader(request: HTTPRequest) -> HTTPResponse {
-//     return h1 {
-//         img { src = "octopus.png"}
-//     }
-//   //return "<h1> <img src='octopus.png' width='100' height='100' alt='🐙' /> Inklings</h1>";
-// }
+func showPage(_ pageID: Int) {
+  do {
+    let pages = Table("pages");
+    let id = Expression<Int>("pageID")
+    let title = Expression<String>("title")
+    let body = Expression<String>("body")
+
+    let db = try Connection("inklings.sqlite3");
+    let query = pages.where(id == pageID)
+    let page = try db.pluck(query)!
+
+    h2 {
+      inner = page[title]
+    }
+    p {
+      classs = "notebook"
+      inner = page[body]
+    }
+   } catch {
+    //log this probably
+  }
+}
+
+enum Roles: Int {
+  case writer = 1
+  case editor
+  case beta
+  case reader
+}
+
+func showNotebooks(forUser user: Int) {
+  do {
+    let user_stories = Table("user_stories");
+    let stories = Table("stories");
+
+    let storyID = Expression<Int>("storyID")
+    let userID = Expression<Int>("userID")
+    let role = Expression<Int>("role")
+    let title = Expression<String>("title")
+    let description = Expression<String>("description")
+
+    let db = try Connection("inklings.sqlite3");
+    let notebookStories = user_stories.where(userID == user && role == Roles.writer.rawValue)
+    for story in try db.prepare(notebookStories) {
+      let notebooks = stories.where(storyID == story[storyID])
+      for notebook in try db.prepare(notebooks) {
+        h2 {
+          inner = "\(notebook[title])"
+        }
+        p {
+          inner = notebook[description]
+        }
+      }
+    }
+   } catch {
+    //log this probably
+  }
+}
